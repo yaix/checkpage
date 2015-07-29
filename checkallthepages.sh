62c761539403a1ca53e8936d10954cdcd54ab84b
Christian Steimel <me@C14L.com>
2015-07-29 15:58:57 +0200

    Initial commit

---
+++ checkallthepages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# checkpage -v http://www.graniteland.com/ 56214db6cfee7cfb92cf92b55a2af77c
+# checkpage -v http://www.graniteland.de/ e3c639d12f645c00dda5cac24bbb2162
+echo "Skip graniteland.com and graniteland.de because they have dynamic markup!"
+echo
+checkpage -v http://www.natursteinkueche.de/ 9aada8f7fb45117d0e33380032f39e4f
+checkpage -v http://www.forumchina.de/ c7a17c1fbf76f18b7fa95a1ff9c41ca6
+checkpage -v http://www.china9.de/ 165de8e087b6a962ff44ccbf25e07671
+checkpage -v http://www.peking-china.de/ ba4f97140bbc25dc3595dd7829f204e9
+checkpage -v http://www.shanghai-china.de/ ac600edf64a3dbbfe1dea440367de690
+checkpage -v http://www.tibet-china.de/ 4003c0adc4d9ec5657c0b06f495dca66
+checkpage -v http://www.hainan-china.de/ 960a656b5d86a8242232ce80772279cd
+checkpage -v http://www.yunnan-china.de/ bc6d2bb89f32bd715fb9ae2043948b3b
+checkpage -v http://www.sunofcancun.com/ e03dbcb49aa267881acd4510be275a9b
+checkpage -v http://www.c14l.com/ 0f9064f328782cefad2130c66cd6704c
+checkpage -v http://www.yaix.de/ bc4b4a5bcbf1625bab918c2a5e04b729
+checkpage -v http://www.messen-kalender.de/ 16d00bc05991f1266a11fe8c4c8105e8 # updated: 2015-07-24
+checkpage -v http://www.stone-installer.com/ ed5febcfadd244d0face55122ff85f8a
+checkpage -v http://www.paulrich.net/ 140c17d136e83a9daf656939d7fb295a
+checkpage -v http://pre.gunta.me/ 00727104948b5b4e0deff3e9011f6e55
+checkpage -v http://www.antrito.com/ bc4b4a5bcbf1625bab918c2a5e04b729
+# checkpage -v http://www.elligue.com/ 
---
+++ checkpage.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+# TODO:
+# ------------------------------------------------------------------------------
+# Parse the markup of the returned page content with beautifulsoup and exclude
+# selected objects. For example, if a page has a certain <div> with non-static
+# content, the user can supply a DOM selector for that <div> and it will be
+# removed from the HTML before the page content is passed to the md5 hasher.
+# Could use --exclude -x argument with a supplied string, and should accept more
+# than one. Example: checkpage --exclude "#article > .current-users" http://...
+# ------------------------------------------------------------------------------
+#
+#
+
+import hashlib
+import optparse
+import urllib.request
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+def main():
+    p = optparse.OptionParser(
+        description='''Verifies that a web page is online and unchanged. 
+
+Returns "1" if the supplied hash value matches the hash value of the page 
+content retreived from the suplied URL.
+
+Returns "0" if a different hash value was found, or if the web site does not 
+reply with an HTTP 200 status code.
+
+Returns "ERROR" in case of DNS error, possibly because the local machine is not
+connected to the internet. These errors are likely unrelated to the status of 
+the target web page.
+        ''',
+        prog='websitecheck',
+        version='websitecheck 0.1',
+        usage='%prog [-v] http://www.example.com/ _KNOWN_EXPECTED_MD5_HASH_STRING_')
+
+    p.add_option('--verbose', '-v', action="store_true", default=False)
+    #p.add_option('--exclude', '-x', default=None) ---TODO!---
+    options, arguments = p.parse_args()
+
+    if len(arguments) in [1,2]:
+        url = arguments[0]
+
+        try:
+            with urllib.request.urlopen(url) as f:
+                content = f.read()
+                page_hash = hashlib.md5(content).hexdigest()
+        except HTTPError: # not a 200
+            if options.verbose:
+                print('ERROR: The page is not reachable, NO HTTP 200 returned!')
+            else:
+                print('0')
+            exit(0)
+        except URLError: # lookup failed, no internet?
+            if options.verbose:
+                print('NETWORK FAILURE: No internet access found, could not request the page.')
+            else:
+                print('NETWORK FAILURE')
+            exit(0)
+
+        if len(arguments) == 1: # only return a hash for that page
+            print(page_hash)
+    
+        elif len(arguments) == 2: # check if hashes are equal
+            expected_hash = arguments[1]
+            if expected_hash == page_hash:
+                if options.verbose:
+                    print('OKAY: hash "{}" matches page content for {}'.format(expected_hash, url))
+                else:
+                    print('1')
+            else:
+                if options.verbose:
+                    print('WARNING: hash "{}" does NOT match page content for {}'.format(expected_hash, url))
+                else:
+                    print('0')
+
+    else: # otherwise return help text
+        p.print_help()
+
+if __name__ == '__main__':
+    main()
